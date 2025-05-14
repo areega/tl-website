d5f456144e0f4f1bf64db73da8962c77eca4032c
lincmba <lincolncmba@gmail.com>
2025-05-15 01:27:16 +0300

    Use vite gh name

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -79,7 +79,7 @@ const Hero: React.FC = () => {
             <div className="relative">
               <div className="aspect-[3/4] rounded-2xl overflow-hidden shadow-xl">
                 <img 
-                  src="/images/tl1.jpeg" 
+                  src="tl1.jpeg" 
                   alt="Diana Towett, Licensed Counselling Psychologist" 
                   className="w-full h-full object-cover object-center hover:scale-105 transition-transform duration-700"
                 />
